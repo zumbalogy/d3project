e19830d83116363d7852396740a362986797f845
zumbalogy <intrapostal@gmail.com>
2014-02-20 18:58:00 -0500

    app.js now builds hash with everyones commit count

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,16 @@ console.log('hello');
 
 
 console.log(raw_log);
+console.log(raw_log[0]);
+
+var alist = {};
+
+for (var i = 0; i < raw_log.length; i++){
+    if (alist[raw_log[i].author]) {
+        alist[raw_log[i].author] ++;
+    } else {
+        alist[raw_log[i].author] = 1;
+    }
+}
+
+console.log(alist)
