254f1c3239fe1456b0976f4277bb7e18adc9d2b7
zumbalogy <intrapostal@gmail.com>
2014-02-21 15:22:32 -0500

    circles working

--- scripts/pie.js
+++ scripts/pie.js
@@ -1,57 +1,59 @@
 var pieData = by_name;
-
 var name;
 
 var key_array = [];
 for (var key in pieData){
-    if (key !== "Lior Elrom <liormb@users.noreply.github.com>" || key !== "dlopezh <daniellopezh@gmail.com>"){
+    if (key == "Lior Elrom <liormb@users.noreply.github.com>" || key == "dlopezh <daniellopezh@gmail.com>"){
+        console.log(key)
+    }
+    else {
         key_array.push(key)
     }
 }
 
-
 function newPiePerson(){
     var key = key_array[Math.floor(Math.random()*key_array.length)];
     name = pieData[key].name;
-    var rubyFiles = pieData[key].ruby_count;
-    var jsFiles = pieData[key].js_count;
-    var htmlFiles = pieData[key].html_count;
-    var erbFiles = pieData[key].erb_count;
-    var cssFiles = pieData[key].css_count;
-    var otherFiles = pieData[key].other_count;
-    return [rubyFiles, jsFiles, htmlFiles, erbFiles, cssFiles, otherFiles];
+    return [
+        pieData[key].ruby_count,
+        pieData[key].js_count,
+        pieData[key].html_count,
+        pieData[key].erb_count,
+        pieData[key].css_count,
+        pieData[key].other_count
+    ]
 };
 
-// window.setInterval(function(){
-    var masterPie = newPiePerson()
-    console.log(name)
+var pie = d3.layout.pie();
+var color = d3.scale.category10();
 
-    var pie = d3.layout.pie();
-    var color = d3.scale.category10();
+var w = 600;
+var h = 600;
 
-    var w = 600;
-    var h = 600;
+var outerRadius = w / 2;
+var innerRadius = 150;
 
-    var outerRadius = w / 2;
-    var innerRadius = 150;
+var arc = d3.svg.arc()
+                .innerRadius(innerRadius)
+                .outerRadius(outerRadius);
 
-    var arc = d3.svg.arc()
-                    .innerRadius(innerRadius)
-                    .outerRadius(outerRadius);
+var svg = d3.select("#pie-chart")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
 
-    var svg = d3.select("#pie-chart")
-                .append("svg")
-                .attr("width", w)
-                .attr("height", h);
+function pieFill(){
+    $('svg').empty()
+    var arcs = svg.selectAll("g.arc").data(pie(newPiePerson()))
 
-    var arcs = svg.selectAll("g.arc")
-            .data(pie(newPiePerson()))
-            .enter()
-            .append("g")
-            .attr("class", "arc")
-            .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")")
+    arcs.enter()
+        .append("g")
+        .attr("class", "arc")
+        .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")")
 
+    arcs.empty();
     arcs.append("path")
+        //.transition()
         .attr("fill", function(d, i) {
             return color(i)
         })
@@ -65,17 +67,13 @@ function newPiePerson(){
         .text(function(d) {
             return d.value;
         });
-
+    
+}
 
 window.setInterval(function(){
-    console.log(arc)
-    svg.selectAll("g.arc")
-            .data(pie(newPiePerson()))
-            .enter()
-            .append("g")
-            .attr("class", "arc")
-            .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")")
-    }, 1000
+        pieFill()
+    }, 
+    1000
 )
 
 
